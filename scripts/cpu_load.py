4a8af0ba4ec00d6f0d0dfa72753b4db715021235
azaa1 <azaa.chi11@gmail.com>
2020-12-23 19:38:16 -0600

    cpu_load.py modified

--- scripts/cpu_load.py
+++ scripts/cpu_load.py
@@ -1,17 +1,4 @@
 # Check System CPU and RAM
 
-import os
-import subprocess
-import re
-
-statistics = {}
-
-cpu_count = os.cpu_count()
-statistics['cpu_count'] = cpu_count
-
-"""
-# Load Average
-"""
-
-cpu_load = [x / os.cpu_count() * 100 for x in os.getloadavg()[-1]]
-statistics['cpu_load'] = cpu_load
+import psutil 
+print(psutil.virtual_memory())
